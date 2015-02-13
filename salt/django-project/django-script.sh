d5b8474a53565a359fb0843320122960083ce4d2
Igor Támara <igor@axiacore.com>
2015-02-13 17:05:00 -0500

    master  Added known_hosts to automatic server deployment

--- salt/django-project/django-script.sh
+++ salt/django-project/django-script.sh
@@ -4,6 +4,7 @@
 cat /dev/zero | ssh-keygen -t rsa -b 8192 -f /opt/deploy/.ssh/id_rsa -q -N ""
 
 # Adds the new key as deployment
+curl https://raw.githubusercontent.com/AxiaCore/public-keys/master/known_hosts > /opt/deploy/.ssh/known_hosts 2>/dev/null
 REPO="https://bitbucket.org/api/1.0/repositories/axiacore/$REPOSLUG/deploy-keys"
 PUB_KEY=`cat /opt/deploy/.ssh/id_rsa.pub`
 curl -v --request POST --user $USERNAME:$PASSWORD \
