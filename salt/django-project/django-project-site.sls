71bec33ab99fd0dd8301d94681cc041fddd0c48d
Igor Támara <igor@axiacore.com>
2015-02-13 12:35:28 -0500

    master  Added brand new key for deployment

--- salt/django-project/django-project-site.sls
+++ salt/django-project/django-project-site.sls
@@ -1,11 +1,15 @@
-/srv/salt/django-project/django-script.sh:
+fetch-and-publish:
     cmd.run:
+        - name: /srv/salt/django-project/django-script.sh
+        - user: deploy
+        - cwd: /opt/deploy
         - env:
-            - 'USERNAME': '{{ pillar['bb_user'] }}'
-            - 'PASSWORD': '{{ pillar['bb_pass'] }}'
-            - 'REPO': '{{ pillar['project_repo'] }}'
+           - USERNAME: {{ pillar['bb_user'] }}
+           - PASSWORD: {{ pillar['bb_pass'] }}
+           - REPOSLUG: '{{ pillar['repo_slug'] }}'
 
     git.latest:
         - name: {{ pillar['project_repo'] }}
         - target: /opt/deploy/{{ pillar['project_name'] }}_app/{{ pillar ['project_name'] }}
         - rev: master
+        - identity: /opt/deploy/.ssh/id_rsa
--- salt/django-project/django-script.sh
+++ salt/django-project/django-script.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
 # Generating ssh keys
-cat /dev/zero | ssh-keygen -t rsa -b 8192 -f /opt/deploy/.ssh/id_rsa
+cat /dev/zero | ssh-keygen -t rsa -b 8192 -f /opt/deploy/.ssh/id_rsa -q -N ""
 
-# Publishing ssh keys
-CURLRESULT=$(curl -v --request POST --user $USERNAME:$PASSWORD \
-  $REPO --header "Content-Type: application/json" --header "Accept: application/json" -d "{\"key\": \"`cat /opt/deploy/.ssh/id_rsa.pub`\"}" 2>/dev/null)
+# Adds the new key as deployment
+REPO="https://bitbucket.org/api/1.0/repositories/axiacore/$REPOSLUG/deploy-keys"
+PUB_KEY=`cat /opt/deploy/.ssh/id_rsa.pub`
+curl -v --request POST --user $USERNAME:$PASSWORD \
+  $REPO --header "Content-Type: application/json" \
+  --header "Accept: application/json" \
+  -d "{\"key\": \"$PUB_KEY\"}" 2>/dev/null
