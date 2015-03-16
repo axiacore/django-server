23688397648d729f115a179a47fa89cae6a6f4c1
Camilo Nova <camilo.nova@gmail.com>
2015-03-16 09:53:47 -0500

    Update django-pip.sh

--- salt/django-project/django-pip.sh
+++ salt/django-project/django-pip.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-source "/opt/deploy/"$PROJECTNAME"_app/bin/activate"
-REQFILE="/opt/deploy/"$PROJECTNAME"_app/"$PROJECTNAME"/requirements.txt"
+source "/opt/deploy/"$PROJECT_NAME"_app/bin/activate"
+REQFILE="/opt/deploy/"$PROJECT_NAME"_app/"$PROJECT_NAME"/requirements.txt"
 pip install -r $REQFILE 
