2b70519499f35eb46ffa0d8dc78cddd924699b69
Camilo Nova <camilo.nova@gmail.com>
2015-03-16 09:52:55 -0500

    Update django-script.sh

--- salt/django-project/django-script.sh
+++ salt/django-project/django-script.sh
@@ -4,13 +4,12 @@
 cat /dev/zero | ssh-keygen -t rsa -b 8192 -f /opt/deploy/.ssh/id_rsa -q -N ""
 
 # Adds the new key as deployment
-curl https://raw.githubusercontent.com/AxiaCore/public-keys/master/known_hosts > /opt/deploy/.ssh/known_hosts 2>/dev/null
-REPO="https://bitbucket.org/api/1.0/repositories/axiacore/$REPOSLUG/deploy-keys"
+curl $KNOWN_HOSTS_URL > /opt/deploy/.ssh/known_hosts 2>/dev/null
+REPO="https://bitbucket.org/api/1.0/repositories/axiacore/$BITBUCKET_REPO/deploy-keys"
 PUB_KEY=`cat /opt/deploy/.ssh/id_rsa.pub`
 curl -v --request POST --user $USERNAME:$PASSWORD \
   $REPO --header "Content-Type: application/json" \
   --header "Accept: application/json" \
   -d "{\"key\": \"$PUB_KEY\"}" 2>/dev/null
 
-DIRNAME="/opt/deploy/"$PROJECTNAME"_app/ws"
-mkdir $DIRNAME
+mkdir /opt/deploy/"$PROJECT_NAME"_app/
